87ed59302fb917bdd15c208d8e7d3ffccd2b39bd
emrul <emrulhasanrony@gmail.com>
2018-12-13 00:03:26 +0600

    13-12-18

--- DataStructure&Algo/LinkedListAll.py
+++ DataStructure&Algo/LinkedListAll.py
@@ -14,6 +14,37 @@ class LinkedList:
         self.head=objNode
 
 # Adding a node after a given node
+    def insertAfter(self,prevNode,newData):
+        newNode = Node(newData)
+
+        if (prevNode==None):
+            print("Privious node must be in the link list")
+            return
+        newNode.next=prevNode.next
+        prevNode.next=newNode
+
+#Adding a node at the end of the linked list
+
+    def append(self,newData):
+
+        newNode=Node(newData)
+
+        if self.head is None:
+            self.head=newNode
+        last=self.head
+        while(last.next):
+            last=last.next
+
+        last.next=newNode
+# Printing the Linked list
+
+    def printList(self):
+        temp=self.head
+        while(temp):
+            print (temp.data)
+            temp=temp.next
+
+
 llist = LinkedList()
 
 llist.head=Node(5)
@@ -22,5 +53,5 @@ third =Node(5)
 
 llist.head.next=second
 second.next=third
-
-llist.push(100)
+llist.append(500)
+llist.printList()

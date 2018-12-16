caf71d4f48f4492a7a968c07934f2fc327849a9f
emrul <emrulhasanrony@gmail.com>
2018-12-17 01:05:41 +0600

    modified:   DataStructure&Algo/LinkedListAll.py 	new file:   DataStructure&Algo/temp.py

--- DataStructure&Algo/LinkedListAll.py
+++ DataStructure&Algo/LinkedListAll.py
@@ -1,57 +1,167 @@
 class Node:
-    def __init__(self,data=None):
-        self.data=data
-        self.next=None
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
 
-class LinkedList:
+
+class LinkedList(Node):
     def __init__(self):
-        self.head=None
+        self.head = None
 
-# Adding a data at the start of the list
-    def push(self,newData):
-        objNode = Node(newData)
-        objNode.next=self.head
-        self.head=objNode
+    def add(self,newData):
 
-# Adding a node after a given node
-    def insertAfter(self,prevNode,newData):
         newNode = Node(newData)
 
-        if (prevNode==None):
-            print("Privious node must be in the link list")
-            return
-        newNode.next=prevNode.next
-        prevNode.next=newNode
-
-#Adding a node at the end of the linked list
-
-    def append(self,newData):
-
-        newNode=Node(newData)
-
-        if self.head is None:
+        if (self.head==None):
             self.head=newNode
-        last=self.head
-        while(last.next):
+
+    # Adding a data at the start of the list
+
+    def push(self, newData):
+        objNode = Node(newData)
+        objNode.next = self.head
+        self.head = objNode
+
+        # Adding a node after a given node
+
+    def addafter(self, prev, newData):
+        newNode = Node(newData)
+
+        if (prev == None):
+            print("The previous node should be in the linked list")
+
+        newNode.next = prev.next
+        prev.next = newNode
+
+        # Adding a node at the end of the list
+
+    def add(self,newData):
+        newNode= Node(newData)
+
+
+        if (self.head==None):
+            self.head=newNode
+            return
+
+        last = self.head
+
+        while (last.next!=None):
             last=last.next
 
         last.next=newNode
-# Printing the Linked list
 
-    def printList(self):
-        temp=self.head
-        while(temp):
-            print (temp.data)
+        # Delete a node by given value of the node
+
+    def deleteNodeByKey(self,key):
+
+        temp = self.head
+
+        if (temp!=None):
+            if (temp.data==key):
+                self.head=temp.next
+                temp==None
+                return
+
+        while (temp):
+            if (temp.data==key):
+                break
+
+            prev = temp
             temp=temp.next
 
+        if (temp==None):
+            print("The data is not in the liked list")
+            return
 
-llist = LinkedList()
 
-llist.head=Node(5)
-second =Node(4)
-third =Node(5)
+        prev.next=temp.next
+        temp=None
 
-llist.head.next=second
-second.next=third
-llist.append(500)
-llist.printList()
+        #Delete a node by given index num
+
+    def delByIndex(self,indexNumber):
+
+        if (self.head==None):
+            return
+        temp = self.head
+
+        if (indexNumber==0):
+            self.head = temp.next
+            temp = None
+            return
+
+
+        # Find the previous number of the number we want to delete
+        for i in range (0,indexNumber-1):
+            temp=temp.next
+            if (temp==None):
+                break
+        # If index number is more than the length of the list
+        if temp==None:
+            return
+        if temp.next==None:
+            return
+        next = temp.next.next
+
+        temp.next=None
+
+        temp.next=next
+
+    # Print the linked list
+    def print(self):
+
+        temp=self.head
+
+        if (self.head==None):
+            print("The list is empty")
+            return
+
+        while(temp.next):
+            print(temp.data, end=" ")
+            temp=temp.next
+        print(temp.data, end=" ")
+    # Count the indexes of node
+
+    def count(self):
+
+        count = 0
+        temp = self.head
+        while(temp!=None):
+            temp=temp.next
+            count=count+1
+
+        print(count)
+
+    def search(self,searchData):
+
+        temp = self.head
+        if (temp==None):
+            return
+        while(temp!=None):
+            if (temp.data==searchData):
+                return True
+            temp=temp.next
+        return False
+
+class main():
+
+    llist = LinkedList()
+    llist.add(6)
+    llist.add(7)
+    llist.add(8)
+    llist.add(10)
+
+    llist.addafter(llist.head.next.next.next,'X')
+    print('Before deleting list')
+    llist.print()
+    print()
+    print('after deleting list')
+    # llist.deleteNodeByKey(10)
+    # llist.delByIndex(2)
+    llist.print()
+    print()
+    llist.count()
+    print(llist.search(10))
+
+main()
+
